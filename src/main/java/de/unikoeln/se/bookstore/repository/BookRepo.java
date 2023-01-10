cb97aed11cb264b32c4b3eaf9cd27c1c874becd0
Yusuf <yusufshehadeh@gmail.com>
2023-01-10 18:58:59 +0100

    Initial commit

---
+++ src/main/java/de/unikoeln/se/bookstore/repository/BookRepo.java
@@ -0,0 +1,8 @@
+package de.unikoeln.se.bookstore.repository;
+
+import de.unikoeln.se.bookstore.datamodel.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepo extends JpaRepository<Book, Integer> {
+
+}
